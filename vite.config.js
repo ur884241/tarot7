69f001683f1bbd355bc723f5a0c74ba0579077b3
ur884241 <jordi1@outlook.com.br>
2025-03-03 04:05:26 +0000

    full

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -20,5 +21,11 @@ export default defineConfig({
         '.jsx': 'jsx'
       }
     }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx', '.json']
   }
 });
