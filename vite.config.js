5d57fa5dff7ec2e83be4f525ff3a830faa21b7ee
ur884241 <jordi1@outlook.com.br>
2025-03-03 03:13:10 +0000

    adding

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist', // Output directory for the build
     emptyOutDir: true, // Clear the output directory before building
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'public/index.html'), // Explicitly specify the entry point
+      },
+    },
   },
   publicDir: 'public', // Specify the public directory
 });
