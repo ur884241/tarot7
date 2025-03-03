e4208b180a9f91dac5b98f547efebed49d15b7d8
ur884241 <jordi1@outlook.com.br>
2025-03-03 03:34:26 +0000

    new

--- src/Index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles/main.css';
-
-const container = document.getElementById('root');
